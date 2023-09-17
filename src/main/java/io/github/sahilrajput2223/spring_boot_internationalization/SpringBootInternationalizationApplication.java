ba0ec5973ecaf51ecff046a40361e5148262fad7
Sahil Rajput <rajputsahil.2204@gmail.com>
2023-09-17 11:10:25 +0530

    Project Started and base files added

---
+++ src/main/java/io/github/sahilrajput2223/spring_boot_internationalization/SpringBootInternationalizationApplication.java
@@ -0,0 +1,13 @@
+package io.github.sahilrajput2223.spring_boot_internationalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInternationalizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInternationalizationApplication.class, args);
+	}
+
+}
